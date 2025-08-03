adf6a54fa8ffe0dbbc107855fe47e097661ad002
SpencerF718 <spencer.flaharty@gmail.com>
2025-08-03 18:55:15 -0400

    Add get_output_path
    Added a function which gets the output directory from user input.  If it does not already exist, then it creates one.

--- markdown_ingest.py
+++ markdown_ingest.py
@@ -8,7 +8,7 @@ def get_vault_path():
 
     while not vault_found:
         vault_path = input(
-            r"Please enter the absolute path to your Obsidian vault. e.g. C:\Users\Name\OneDrive\Documents\Main")
+            r"Please enter the absolute path to your Obsidian vault. e.g. C:\Users\Name\Documents\Main")
 
         if os.path.isabs(vault_path):
             vault_found = True
@@ -18,8 +18,37 @@ def get_vault_path():
     return vault_path
 
 
+def get_output_path():
+
+    output_found = False
+    output_path = ""
+
+    while not output_found:
+        output_path = input(
+            r"Please enter the absolute path to your output directory.  e.g. C:\Users\Name\Documents\Output")
+
+        if os.path.isabs(output_path):
+            try:
+                if not os.path.exists(output_path):
+                    os.makedirs(output_path)
+                    output_found = True
+            except OSError as e:
+                print(
+                    f"ERROR: Could not create directory {output_path}.  Because: {e}")
+        else:
+            print(f"ERROR: Invalid path.")
+
+    return output_path
+
+
 def main():
 
     print("Obsidian Markdown Ingester: ")
 
     vault_path = get_vault_path()
+    output_path = get_output_path()
+    print(f"Output path: {output_path}, Vault path: {vault_path}")
+
+
+if __name__ == "__main__":
+    main()
